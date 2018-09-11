59d4527f8e961bdad8ee2d76b8ad55973051fed0
Julius Lungys <juliuslungys@gmail.com>
2018-09-11 18:40:53 +0300

    refactor InstructionLineTest

---
+++ KompiuteriuArchitektura_Lab01Tests/InstructionLineTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "../KompiuteriuArchitektura_Lab01/InstructionLine.cpp"
+
+TEST(InstructionLine, conststructorZeroes) {
+	InstructionLine instructionLine("0", '0', '0', '*', "0");
+	ASSERT_EQ(instructionLine.currentState, "0");
+	ASSERT_EQ(instructionLine.currentSymbol, '0');
+	ASSERT_EQ(instructionLine.nextSymbol, '0');
+	ASSERT_EQ(instructionLine.headMovementDirection, '*');
+	ASSERT_EQ(instructionLine.nextState, "0");
+}
+TEST(InstructionLine, conststructorOnes) {
+	InstructionLine instructionLine("01", '1', '1', '*', "01");
+	ASSERT_EQ(instructionLine.currentState, "01");
+	ASSERT_EQ(instructionLine.currentSymbol, '1');
+	ASSERT_EQ(instructionLine.nextSymbol, '1');
+	ASSERT_EQ(instructionLine.headMovementDirection, '*');
+	ASSERT_EQ(instructionLine.nextState, "01");
+}
+TEST(InstructionLine, equals) {
+	InstructionLine instructionLineA("01", '1', '1', '*', "01");
+	InstructionLine instructionLineB("01", '1', '1', '*', "01");
+
+	ASSERT_TRUE(instructionLineA.equals(instructionLineB));
+}
+TEST(InstructionLine, notEquals) {
+	InstructionLine instructionLineA("01", '1', '1', '*', "05");
+	InstructionLine instructionLineB("01", '1', '1', '*', "01");
+
+	ASSERT_FALSE(instructionLineA.equals(instructionLineB));
+}
--- KompiuteriuArchitektura_Lab01Tests/TuringMachineTest.cpp
+++ KompiuteriuArchitektura_Lab01Tests/TuringMachineTest.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "../KompiuteriuArchitektura_Lab01/TuringMachine.cpp"
-#include "../KompiuteriuArchitektura_Lab01/InstructionLine.cpp"
 #include "../KompiuteriuArchitektura_Lab01/Tape.cpp"
 #include "../KompiuteriuArchitektura_Lab01/Program.cpp"
 #include "../KompiuteriuArchitektura_Lab01/FileInteractor.cpp"
@@ -17,34 +16,7 @@ TuringMachine createTuringMachine(std::string tape, int headPosition) {
 	turingMachine.head = headPosition;
 	return turingMachine;
 }
-TEST(InstructionLine, conststructorZeroes) {
-	InstructionLine instructionLine("0", '0', '0', '*', "0");
-	ASSERT_EQ(instructionLine.currentState, "0");
-	ASSERT_EQ(instructionLine.currentSymbol, '0');
-	ASSERT_EQ(instructionLine.nextSymbol, '0');
-	ASSERT_EQ(instructionLine.headMovementDirection, '*');
-	ASSERT_EQ(instructionLine.nextState, "0");
-}
-TEST(InstructionLine, conststructorOnes) {
-	InstructionLine instructionLine("01", '1', '1', '*', "01");
-	ASSERT_EQ(instructionLine.currentState, "01");
-	ASSERT_EQ(instructionLine.currentSymbol, '1');
-	ASSERT_EQ(instructionLine.nextSymbol, '1');
-	ASSERT_EQ(instructionLine.headMovementDirection, '*');
-	ASSERT_EQ(instructionLine.nextState, "01");
-}
-TEST(InstructionLine, equals) {
-	InstructionLine instructionLineA("01", '1', '1', '*', "01");
-	InstructionLine instructionLineB("01", '1', '1', '*', "01");
 
-	ASSERT_TRUE(instructionLineA.equals(instructionLineB));
-}
-TEST(InstructionLine, notEquals) {
-	InstructionLine instructionLineA("01", '1', '1', '*', "05");
-	InstructionLine instructionLineB("01", '1', '1', '*', "01");
-
-	ASSERT_FALSE(instructionLineA.equals(instructionLineB));
-}
 TEST(TuringMachine, headStartsAtZero) {
 	TuringMachine turingMachine;
 	ASSERT_EQ(turingMachine.head, 0);
