ec2f6e113e8cd38f4f18efd317240d63c9014c8e
Julius Lungys <juliuslungys@gmail.com>
2018-09-11 18:46:00 +0300

    refactor ProgramTest

--- KompiuteriuArchitektura_Lab01Tests/TuringMachineTest.cpp
+++ KompiuteriuArchitektura_Lab01Tests/TuringMachineTest.cpp
@@ -1,5 +1,4 @@
 #include "pch.h"
-#include "../KompiuteriuArchitektura_Lab01/Program.cpp"
 #include "../KompiuteriuArchitektura_Lab01/FileInteractor.cpp"
 #include <vector>
 
@@ -102,30 +101,6 @@ TEST(Program, getLinesOfState) {
 	std::vector<InstructionLine> linesOfState = program.getLinesOfState("0");
 
 	ASSERT_EQ(linesOfState.size(), 2);
-}
-TEST(Program, equals) {
-	Program programA;
-	programA.instructionLines.push_back(InstructionLine("0", '0', '0', 'r', "0"));
-	programA.instructionLines.push_back(InstructionLine("0", '1', '0', 'l', "0"));
-	programA.instructionLines.push_back(InstructionLine("1", 'A', '0', '*', "Halt"));
-	Program programB;
-	programB.instructionLines.push_back(InstructionLine("0", '0', '0', 'r', "0"));
-	programB.instructionLines.push_back(InstructionLine("0", '1', '0', 'l', "0"));
-	programB.instructionLines.push_back(InstructionLine("1", 'A', '0', '*', "Halt"));
-
-	ASSERT_TRUE(programA.equals(programB));
-}
-TEST(Program, notEquals) {
-	Program programA;
-	programA.instructionLines.push_back(InstructionLine("0", '0', '0', 'r', "0D"));
-	programA.instructionLines.push_back(InstructionLine("0", '1', '0', 'l', "0"));
-	programA.instructionLines.push_back(InstructionLine("1", 'A', '0', '*', "Halt"));
-	Program programB;
-	programB.instructionLines.push_back(InstructionLine("0", '0', '0', 'r', "0"));
-	programB.instructionLines.push_back(InstructionLine("0", '1', '0', 'l', "0"));
-	programB.instructionLines.push_back(InstructionLine("1", 'A', '0', '*', "Halt"));
-
-	ASSERT_FALSE(programA.equals(programB));
 }
 TEST(TuringMachineRun, emptyProgram) {
 	TuringMachine turingMachine = createTuringMachine("B00");
